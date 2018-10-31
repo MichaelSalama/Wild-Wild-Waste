e80b1967a27938519d3735f61e910e895bb040c0
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-10-31 10:59:37 +0200

    second initial commit
    Now it is working with unity 2017

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+    [SerializeField]
+    int gameTime;
+
+    [SerializeField]
+    int gameScore;
+
+    void Awake()
+    {
+        if (Instance = null)
+            Instance = this;
+    }
+
+    public void ChangeScrore(int value)
+    {
+        gameScore += value;
+    }
+}
---
+++ Assets/Scripts/IneractableObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IneractableObjects : MonoBehaviour {
+
+    [SerializeField]
+    WasteTypes type;
+
+	void Update () {
+	    	
+	}
+}
+public enum WasteTypes
+{
+    Green,
+    Blue,
+    Red,
+    White
+}
